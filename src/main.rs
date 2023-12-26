d04e57455bbccf11664f969f49d493e31a3cc49d
iamsuneeth <suneeth.lenin@gmail.com>
2023-12-26 20:56:40 +0530

    feat: using clap and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,71 @@
+use anyhow::{Context, Result};
+use clap::Parser;
 use std::{
-    env, fs,
-    io::{self, Read, Result},
+    fs,
+    io::{self, Read},
 };
 
-fn get_content(file_name: Option<&str>) -> Result<String> {
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short = 'c')]
+    bytes: bool,
+    #[arg(short = 'l')]
+    lines: bool,
+    #[arg(short = 'w')]
+    word: bool,
+    #[arg(short = 'm')]
+    characters: bool,
+    #[arg(required = false)]
+    file_name: Option<String>,
+}
+
+fn get_content(file_name: &Option<String>) -> Result<String> {
     let mut input = String::new();
     if let Some(value) = file_name {
-        fs::read_to_string(value)
+        fs::read_to_string(value).with_context(|| format!("Failed to read file {value}"))
     } else {
-        let result = io::stdin().read_to_string(&mut input);
-        if result.is_err() {
-            Err(result.unwrap_err())
-        } else {
-            Ok(input)
-        }
+        io::stdin()
+            .read_to_string(&mut input)
+            .with_context(|| "Failed to read data from standard input")?;
+        Ok(input)
     }
 }
 
-fn get_output(result: String, file_name: Option<&str>) -> String {
+fn get_output(result: String, file_name: &Option<String>) -> String {
     if let Some(value) = file_name {
-        format!("{result} ${value}")
+        format!("{result} {value}")
     } else {
         result
     }
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let flag: Option<&str>;
-    let file_name: Option<&str>;
-    let args_length = args.len();
-    if args_length == 1 {
-        flag = None;
-        file_name = None;
-    } else if args_length == 2 && args[1].starts_with("-") {
-        flag = Some(&args[1]);
-        file_name = None;
-    } else if args_length == 2 {
-        flag = None;
-        file_name = Some(&args[1]);
-    } else {
-        flag = Some(&args[1]);
-        file_name = Some(&args[2]);
-    }
+fn main() -> Result<()> {
+    let Args {
+        bytes,
+        lines,
+        word,
+        characters,
+        file_name,
+    } = Args::parse();
 
-    let contents = get_content(file_name).expect("Cannot read file");
-    let result = match flag {
-        Some("-c") => contents.bytes().count().to_string(),
-        Some("-l") => contents.lines().count().to_string(),
-        Some("-w") => contents.split_whitespace().count().to_string(),
-        Some("-m") => contents.chars().count().to_string(),
-        Some(_) => panic!("Not implemented option"),
-        None => {
-            let byte_count = contents.bytes().count();
-            let line_count = contents.lines().count();
-            let word_count = contents.split_whitespace().count();
-            format!("{line_count}\t{word_count}\t{byte_count}")
-        }
+    let contents = get_content(&file_name)?;
+    let byte_count = contents.bytes().count();
+    let line_count = contents.lines().count();
+    let word_count = contents.split_whitespace().count();
+    let character_count = contents.chars().count();
+    let format_string = if bytes {
+        format!("{byte_count}")
+    } else if lines {
+        format!("{line_count}")
+    } else if word {
+        format!("{word_count}")
+    } else if characters {
+        format!("{character_count}")
+    } else {
+        format!("{line_count}\t{word_count}\t{byte_count}")
     };
-    let output = get_output(result, file_name);
+    let output = get_output(format!("{}", format_string), &file_name);
     println!("{}", output);
+    Ok(())
 }
